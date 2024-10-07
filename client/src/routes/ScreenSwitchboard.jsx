fc0126e3e140464d7ecb5f78453e4fa4b42de99e
VladislavSCV <vladnety134@gmail.com>
2024-10-07 09:16:29 +0300

    исправил баги

--- client/src/routes/ScreenSwitchboard.jsx
+++ client/src/routes/ScreenSwitchboard.jsx
@@ -1,6 +1,5 @@
-// eslint-disable-next-line no-unused-vars
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 import Groups from "../screens/Groups/Groups";
 import StudentsList from "../screens/StudentsList/StudentsList";
@@ -16,46 +15,46 @@ import CuratorGroupsNotes from "../screens/CuratorGroupsNotes/CuratorGroupsNotes
 import CuratorGroupsSchedule from "../screens/CuratorGroupsSchedule/CuratorGroupsSchedule";
 import CuratorGroupsStudentsList from "../screens/CuratorGroupsStudentsList/CuratorGroupsStudentsList";
 import AdminPanel from "../screens/AdminPanel/AdminPanel";
-// import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const ScreenSwitchboard = () => {
-  // const isAuth = useSelector((state) => state.user.isAuth);
+  const isAuth = useSelector((state) => state.user.isAuth);
   return (
-    <>
-      {/*{!isAuth ? (*/}
-      {/*  <Routes>*/}
-      {/*    <Route path="/main" element={<Main />} />*/}
-      {/*    <Route path="*" element={<Navigate to="/main" />} />*/}
-      {/*  </Routes>*/}
-      {/*) : (*/}
-        <Routes>
-          <Route path="/main" element={<Main />} />
-          <Route path="/groups" element={<Groups />} />
-          <Route path="/curatorgroups/*" element={<CuratorGroups />} />
-          <Route path="/studentsList" element={<StudentsList />} />
-          <Route path="/schedule" element={<Schedule />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/studentAttendance" element={<StudentAttendance />} />
-          <Route path="/notes" element={<Notes />} />
-          <Route path="/GroupsNotes" element={<GroupsNote />} />
-          <Route path="/GroupsSchedule" element={<GroupsSchedule />} />
-          <Route path="/GroupsStudentsList" element={<GroupsStudentsList />} />
-          <Route path="/CuratorGroupsNotes" element={<CuratorGroupsNotes />} />
-          <Route
-            path="/CuratorGroupsSchedule"
-            element={<CuratorGroupsSchedule />}
-          />
-          <Route
-            path="/CuratorGroupsStudentsList"
-            element={<CuratorGroupsStudentsList />}
-          />
-          <Route
-            path="/AdminPanel"
-            element={<AdminPanel />}
-          />
-        </Routes>
-      {/*)}*/}
-    </>
+      <>
+        {!isAuth ? (
+            <Routes>
+              <Route path="/main" element={<Main />} />
+              <Route path="*" element={<Navigate to="/main" />} />
+            </Routes>
+        ) : (
+            <Routes>
+              <Route path="/main" element={<Main />} />
+              <Route path="groups" element={<Groups />} />
+              <Route path="/curatorgroups/*" element={<CuratorGroups />} />
+              <Route path="/studentsList" element={<StudentsList />} />
+              <Route path="/schedule" element={<Schedule />} />
+              <Route path="/main" element={<Main />} />
+              <Route path="/studentAttendance" element={<StudentAttendance />} />
+              <Route path="/notes" element={<Notes />} />
+              <Route path="/GroupsNotes" element={<GroupsNote />} />
+              <Route path="/GroupsSchedule" element={<GroupsSchedule />} />
+              <Route path="/GroupsStudentsList" element={<GroupsStudentsList />} />
+              <Route path="/CuratorGroupsNotes" element={<CuratorGroupsNotes />} />
+              <Route
+                  path="/CuratorGroupsSchedule"
+                  element={<CuratorGroupsSchedule />}
+              />
+              <Route
+                  path="/CuratorGroupsStudentsList"
+                  element={<CuratorGroupsStudentsList />}
+              />
+              <Route
+                  path="/AdminPanel"
+                  element={<AdminPanel />}
+              />
+            </Routes>
+        )}
+      </>
   );
 };
 
